c581c3e2eebe1927d91487465df9161a99601d9a
marothon <marothon@gmail.com>
2024-10-17 14:55:06 +0200

    Add functionality to edit deck name

--- src/pages/DeckDetailPage.tsx
+++ src/pages/DeckDetailPage.tsx
@@ -2,13 +2,16 @@ import { useLoaderData, useNavigate } from "react-router-dom"
 import { DeckData } from "../shared/interfaces";
 import { DeckCardList, DeckContext, ManaCostBarChart } from "../components/deck";
 import './css/DeckDetailPage.css';
-import { useContext, useEffect, useState } from "react";
+import { FormEventHandler, useContext, useEffect, useRef, useState } from "react";
 import { translateSymbols } from "../shared/data";
 
 export function DeckDetailPage() {
   const pagedDeck: DeckData = useLoaderData() as DeckData;
   const {deck, changeDeck} = useContext(DeckContext);
   const navigate = useNavigate();
+  const [isEditingName, setIsEditingName] = useState<boolean>(false);
+  const [deckName, setDeckName] = useState<string>(pagedDeck.name);
+  const deckNameRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if(!pagedDeck){
@@ -17,13 +20,47 @@ export function DeckDetailPage() {
     changeDeck(pagedDeck);
   }, []);
 
+  useEffect(() => {
+    const deckWithChangedName = structuredClone(pagedDeck);
+    deckWithChangedName!.name = deckName;
+    changeDeck(deckWithChangedName);
+  }, [deckName]);
+
+  useEffect(() => {
+    deckNameRef.current?.focus();
+  }, [deckNameRef.current])
+
+  const onNameChange: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const formData = new FormData((e.target as HTMLFormElement));
+    if(formData.get("deckName")){
+      setDeckName(formData.get("deckName") as string);
+      setIsEditingName(false);
+    }
+  }
+  
   return (
     <div className='deck-detail-page'>
       {
         deck ?
         <>
           <div className='deck-banner'>
-            <h1>{deck.name}</h1>
+            <div className="deck-name">
+              {
+                isEditingName ?
+                  <>
+                    <form onSubmit={onNameChange}>
+                      <input name="deckName" ref={deckNameRef} defaultValue={deck.name}/>
+                      <span className='material-symbols-outlined' onClick={()=>{setIsEditingName(false)}}>cancel</span>
+                    </form>
+                  </>
+                :
+                  <>
+                    <h1>{deck.name}</h1>
+                    <span className='material-symbols-outlined' onClick={()=>{setIsEditingName(true)}}>edit_square</span>
+                  </>
+              }
+            </div>
             <section className='color-identity'>
               {
                 deck.color_identity.map(c => translateSymbols(`{${c}}`))
