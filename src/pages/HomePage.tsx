82819fa575ab18a4eaaca6337ff4f1719581c38f
marothon <marothon@gmail.com>
2024-10-21 09:46:33 +0200

    Add About-section content in the HomePage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -32,12 +32,20 @@ export function HomePage() {
       <section className="about">
         <h1>About</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non massa quis erat commodo accumsan.
-          Pellentesque finibus mauris eu ligula aliquam laoreet ut eget nisi. Quisque sit amet velit risus.
-          Vivamus vulputate, ligula sit amet tristique dapibus, sapien ex scelerisque ex, eu tristique dui arcu feugiat turpis.
-          Fusce vel blandit enim. Aliquam dapibus ligula a facilisis aliquam. Proin euismod urna pellentesque massa pulvinar dapibus.
-          Donec a justo euismod, maximus purus et, ultricies metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-          Vestibulum ut blandit est. Integer nibh velit, congue vitae dui quis, facilisis mollis dolor. Ut nec dictum ipsum. Praesent ac blandit enim, at euismod dolor. Integer sed sagittis libero.
+          Deckia is a <a target="_blank" href="https://react.dev/">React</a> application to build <a target="_blank" href="https://magic.wizards.com/en">Magic: The Gathering</a> decks. It was implemented as the final individual project of the <a target="_blank" href="https://github.com/Lexicon-Frontend-2024">Lexicon 2024 Front-End Program.</a> 
+        </p>
+        <section>
+          <p>
+            Deckia uses the following libraries in addition to React:
+          </p>
+          <ul>
+            <li><a target="_blank" href="https://reactrouter.com/en/main">React Router</a></li>
+            <li><a target="_blank" href="https://recharts.org/en-US/">Recharts</a></li>
+            <li><a target="_blank" href="https://www.npmjs.com/package/react-string-replace">React String Replace</a></li>
+          </ul> 
+        </section>
+        <p>
+          Card search and imagery is provided by <a target="_blank" href="https://scryfall.com/docs/api">Scryfall's API</a>. This application has no affiliation whatsoever with Scryfall.
         </p>
       </section>
       <section className="search">
