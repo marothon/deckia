2a83cd23a705b6f4e3bf7ca96c78edb9b4c3e093
marothon <marothon@gmail.com>
2024-10-21 15:56:39 +0200

    Draft CardSearchPage component

--- src/pages/CardSearchPage.tsx
+++ src/pages/CardSearchPage.tsx
@@ -1,5 +1,33 @@
+import { useContext } from 'react'
+import './css/CardSearchPage.css'
+import { DeckContext } from '../components/deck'
+import { CardSearch } from '../components/card-search';
+import { CardList } from '../components/card-list';
+
 export function CardSearchPage() {
+  const {deck} = useContext(DeckContext);
+
+  const cards = deck ? [...deck.cards.values()].map(c => c.card) : [];
+
   return (
-    <div>CardSearchPage</div>
+    <div className="card-search-page">
+      <section className='deck-edit'>
+      {
+        deck ? 
+          <>
+            <h2>{deck.name}</h2>
+            <CardList listType='row' cards={cards} />
+          </>
+        :
+          <h3>
+            Add cards to create a deck!
+          </h3>
+      }
+      </section>
+
+      <section className="search">
+        <CardSearch />
+      </section>
+    </div>
   )
 }
