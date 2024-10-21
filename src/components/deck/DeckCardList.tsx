f1b860c4a944891a64c8f69ae8e2c639b9aea859
marothon <marothon@gmail.com>
2024-10-21 16:05:26 +0200

    Add filtering of cards on DeckDetailPage

--- src/components/deck/DeckCardList.tsx
+++ src/components/deck/DeckCardList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEventHandler, useState } from "react";
 import { CardList } from "../card-list"
 import { DeckData } from "../../shared/interfaces";
 import './css/DeckCardList.css'
@@ -6,19 +6,28 @@ import { ListTypeToggle } from "../ui";
 
 export function DeckCardList({deck}: {deck: DeckData}) {
   const [listType, setListType] = useState<'card'|'row'>('card');
+  const [filteredTerm, setFilteredTerm] = useState<string>('');
   
   const onToggle = (listType: 'card'|'row') => {
     setListType(listType);
   }
 
-  // Convert the Map of cards to an array of cards
-  const cards = [...deck.cards.values()].map(c => c.card)
+
+  const onFilter:FormEventHandler<HTMLInputElement> = (e) => {
+    setFilteredTerm((e.target as HTMLInputElement).value);
+  }
+
+  // Convert the Map of cards to an array of cards and filter based on filteredName
+  const cards = [...deck.cards.values()].map(c => c.card).filter(
+    (card) => filteredTerm !== '' ? card.name.toLowerCase().includes(filteredTerm.toLocaleLowerCase()) : true
+  );
+
   
   return (
     <div className='deck-card-list'>
       <div className='filter-container'>
         <div className='filter-bar'>
-          <input name='filter-term' type='text' placeholder="Filter"/>
+          <input name='filter-term' type='text' placeholder="Filter" onInput={onFilter}/>
           <span className="search-indicator material-symbols-outlined">search</span>
         </div>
         <ListTypeToggle onToggle={onToggle} />
